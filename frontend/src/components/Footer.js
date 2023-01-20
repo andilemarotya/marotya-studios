dce31b4488e7af4dee81c73224fac646e40405af
Mandile Andile Marotya <marotyamandile@gmail.com>
2023-01-20 02:11:35 +0200

    Redirecting Ts&Cs link to home page

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -36,7 +36,7 @@ const Footer = () => {
                             
                             <a href="mailto:marotyastudios@gmail.com" className="f-mail">marotyastudios@gmail.com</a>
                             <a href="tel:+27729759950" className="f-no">Phone: +27 72 975 9950</a>
-                            <Link to="/Terms" className="terms">Terms & Conditions</Link>
+                            <Link to="/" className="terms">Terms & Conditions</Link>
                         </ul>
                     </ul>
 
